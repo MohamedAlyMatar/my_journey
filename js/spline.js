bd4cca9e458e4c6d7d18c204f002deef8e502b16
“Mohamed <mohamedalymatar2001@gmail.com>
2023-06-27 16:22:43 +0300

    edited spline and site icon

--- js/spline.js
+++ js/spline.js
@@ -2,4 +2,4 @@ import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
-app.load('https://prod.spline.design/3PMfNX1lSevV6idc/scene.splinecode');
+app.load('https://prod.spline.design/7YsD7PJC1rBBDmSE/scene.splinecode');
